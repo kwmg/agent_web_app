8945fd8e16b28af99cebedf4575bcbf5d7ccca3b
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2019-12-09 14:26:35 +0900

    add flask base experiments.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
+import random
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+comedy_movie_ads = ['c1.gif', 'c2.gif', 'c3.gif']
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/show/comedy')
+def show_page():
+    idx = int(random.random() * len(comedy_movie_ads))
+    return render_template("show_movie_ad.html",
+                           target_page="/enq/comedy/{}".format(idx),
+                           img_file=comedy_movie_ads[idx])
+
+
+@app.route('/enq/comedy/<movie_idx>')
+def show_enquete(movie_idx):
+    return render_template("enquete.html", movie_idx=movie_idx)
 
 
 @app.route('/procForm', methods=['POST'])
@@ -19,4 +36,4 @@ def post_action():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port='8000', debug=True)
