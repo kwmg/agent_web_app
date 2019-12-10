e4041ddb93f32243045db610b7f2f47185e6dbb3
megumikawata <megumi.kawata@mulabo.org>
2019-12-10 18:30:07 +0900

    add rate & daialogs & fixed image location

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import random
 from flask import Flask, render_template, redirect, url_for, request, session
 
+from dialogs import dialogs
+
 app = Flask(__name__)
 app.secret_key = 'db295528b34367fa2a5a5ece8217b4b712136c171d8a6c1fca622151736495c0'
 
@@ -9,6 +11,30 @@ REPEAT_NUM = 4
 list_movie_ads = ['c1.gif', 'c2.gif', 'c3.gif', 'h1.gif',
                   'h2.gif', 'h3.gif', 'd1.gif', 'd2.gif', 'd3.gif']
 
+patterns = [["1_m.gif", ""],
+            ["2_m.gif", ""],
+            ["3_m.gif", ""],
+            ["4_m.gif", ""],
+            ["5_f.gif", ""],
+            ["6_f.gif", ""],
+            ["1_m.gif", "5_f.gif"],
+            ["2_m.gif", "5_f.gif"],
+            ["3_m.gif", "5_f.gif"],
+            ["4_m.gif", "5_f.gif"],
+            ["1_m.gif", "6_f.gif"],
+            ["2_m.gif", "6_f.gif"],
+            ["3_m.gif", "6_f.gif"],
+            ["4_m.gif", "6_f.gif"],
+            ["5_f.gif", "1_m.gif"],
+            ["5_f.gif", "2_m.gif"],
+            ["5_f.gif", "3_m.gif"],
+            ["5_f.gif", "4_m.gif"],
+            ["6_f.gif", "1_m.gif"],
+            ["6_f.gif", "2_m.gif"],
+            ["6_f.gif", "3_m.gif"],
+            ["6_f.gif", "4_m.gif"]]
+
+"""
 patterns = [{"settokusha": "1_m.gif", "hisettokusha": ""},
             {"settokusha": "2_m.gif", "hisettokusha": ""},
             {"settokusha": "3_m.gif", "hisettokusha": ""},
@@ -31,6 +57,7 @@ patterns = [{"settokusha": "1_m.gif", "hisettokusha": ""},
             {"settokusha": "6_f.gif", "hisettokusha": "2_m.gif"},
             {"settokusha": "6_f.gif", "hisettokusha": "3_m.gif"},
             {"settokusha": "6_f.gif", "hisettokusha": "4_m.gif"}]
+"""
 
 
 @app.route('/')
@@ -53,22 +80,19 @@ def user_login():
 
 @app.route('/ad')
 def show_ad():
-    movie_list = list(range(len(list_movie_ads)))
-    dup = list(set(movie_list) & set(session['movie_list']))
-    ag_list = list(range(len(patterns)))
-    ns = list(set(ag_list) & set(session['ag_list']))
-    for d in dup:
-        movie_list.remove(d)
-    for n in ns:
-        ag_list.remove(n)
+    movie_list = [m for m in range(
+        len(list_movie_ads)) if m not in session['movie_list']]
+    ag_list = [a for a in range(len(patterns)) if a not in session['ag_list']]
     idx = movie_list[int(random.random() * len(movie_list))]
     pt = ag_list[int(random.random() * len(ag_list))]
+    d = dialogs[int(random.random() * len(dialogs))]
     session['current_movie'] = idx
     session['current_ag'] = pt
     return render_template("show_movie_ad.html",
-                           wait_time=5,  # 秒で指定
+                           wait_time=10,  # 秒で指定
                            img_movie=list_movie_ads[idx],
-                           img_ag=patterns[pt])
+                           img_ag=patterns[pt],
+                           dialog=d)
 
 
 @app.route('/enq')
@@ -81,7 +105,9 @@ def proc_enquete():
     movie_idx = session['current_movie']
     ag_pt = session['current_ag']
     enq_entry = {'idx': movie_idx, 'pt': ag_pt,
-                 'rating': int(request.form['rating'])}
+                 'rating': int(request.form['rating']),
+                 'credi': int(request.form['credi']),
+                 'satis': int(request.form['satis'])}
     enq_res = session['enq_res']
     enq_res.append(enq_entry)
     session['enq_res'] = enq_res
